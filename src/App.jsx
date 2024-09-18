fe5769de067b3c1693b97397f6d9a8549ed6b7d7
Abdullah Hared <muxsinxareed6@gmail.com>
2024-09-18 14:42:58 +0200

    Display countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,57 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const getCountries = () => {
   return axios
-    .get(baseUrl)
-    .then((response) => console.log(response.data))
+    .get('https://restcountries.com/v3.1/all')
+    .then((response) => response.data)
     .catch((error) => console.log("Error: ", error));
 };
-
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState("");
-  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
-    getCountries().then((response) => setCountries(response));
+    getCountries().then((data) => {
+      setCountries(data);
+    });
   }, []);
 
-  const displayCountries = () => {
-    if (search) {
-      const filtered = countries.filter((country) =>
-        country.name.toLowerCase().includes(search.toLowerCase())
-      );
-      setFilteredCountries(filtered);
-    }
+  const printCountries = () => {
+    const filteredCountries = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    filteredCountries.forEach((country) => console.log(country.name.common));
   };
 
   const handleSearch = (e) => {
     e.preventDefault();
     setSearch(e.target.value);
-    console.log(search);
-    displayCountries();
-  }
+  };
+
+  printCountries();
 
   return (
     <div>
       <h1>Countries</h1>
-  </div>
-  )
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Search countries"
+      />
+      <ul>
+        {countries
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+      </ul>
+    </div>
+  );
 };
+
 export default App;
